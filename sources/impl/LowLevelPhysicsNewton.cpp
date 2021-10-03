14facebc3c3d1be4c7bb653868be1e2960aee739
Vasily Postnicov <shamaz.mazum@gmail.com>
2021-10-03 21:27:41 +0300

    Build first test application

--- sources/impl/LowLevelPhysicsNewton.cpp
+++ sources/impl/LowLevelPhysicsNewton.cpp
@@ -16,7 +16,9 @@
  * You should have received a copy of the GNU General Public License
  * along with HPL1 Engine.  If not, see <http://www.gnu.org/licenses/>.
  */
+#ifdef WIN32
 #pragma comment(lib, "newton.lib")
+#endif
 
 #include "impl/LowLevelPhysicsNewton.h"
 
